dc748e0f68b71abac9a83fbde1f28db31e8caa71
elon21 <elongated21@gmail.com>
2021-08-28 22:14:32 +0300

    React.memo

--- src/App.js
+++ src/App.js
@@ -14,10 +14,22 @@ import aws from "./assets/aws.png";
 import axios from "axios";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [img, setImg] = useState(null);
+
+  const increase = () => {
+    setCount(count+1);
+  }
 
   return (
     <div className="App">
-      <UseRefComponent/>
+      {/* <UseRefComponent/> */}
+      <Header img={img} />
+      <p>Count: {count}</p>
+      <button onClick={increase}>Increase</button>
+      <button onClick={() => setImg(fs)}>FS</button>
+      <button onClick={() => setImg(aws)}>AWS</button>
+
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,35 @@
-import React from "react";
+import React, {memo} from "react";
 import cw from "../assets/cw_logo.png";
+
+// const Header = React.memo(({ img }) => {
+//   console.log("Rendering: Header Comp!");
+//   return (
+//     <div className="header">
+//       <img
+//         src={img ? img : cw}
+//         alt="CW_logo"
+//         style={{ margin: "1rem", maxHeight: "200px" }}
+//       />
+//     </div>
+//   );
+// });
+
+// export default Header;
+
+// const Header = ({ img }) => {
+//   console.log("Rendering: Header Comp!");
+//   return (
+//     <div className="header">
+//       <img
+//         src={img ? img : cw}
+//         alt="CW_logo"
+//         style={{ margin: "1rem", maxHeight: "200px" }}
+//       />
+//     </div>
+//   );
+// };
+
+// export default React.memo(Header);
 
 const Header = ({ img }) => {
   console.log("Rendering: Header Comp!");
@@ -14,4 +44,6 @@ const Header = ({ img }) => {
   );
 };
 
-export default Header;
+export default memo(Header);
+
+
